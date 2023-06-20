af4aec2ac79986268830292b589db6ba723e2922
Carl Tristan Altamerano <carlaltamerano0875@gmail.com>
2023-06-20 12:09:06 +0800

    Version 1

---
+++ script.js
@@ -0,0 +1,4 @@
+const content = 'Tristan B'
+const version = 'version 1'
+
+console.log(content, version);
---
+++ src/index.js
@@ -0,0 +1,4 @@
+const content = 'Tristan A'
+const version = 'version 1'
+
+console.log(content, version);
