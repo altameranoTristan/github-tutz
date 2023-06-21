f2a8166981432e8a4f57459a4f2f38589975ad9c
altameranoTristan <137136995+altameranoTristan@users.noreply.github.com>
2023-06-21 12:52:58 +0800

    GitTutz_v6.1

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-const content = 'Tristan B'
-const version = 'version 1'
+const profile = {
+    name : 'Tristan A',
+    age : 24,
+    gender : 'male',
+    hobbies : ['Valorant', 'CODM', 'LOL', 'LOL:WR', 'Outlast Trinity'],
+    status : 'single',
+    level : 'college',
+    skills : ['web development', 'service crew', 'helper', 'staff']
+} 
 
-console.log(content, version);
+const version = {
+    number : 'v6.1',
+    name : 'Git Cloned'
+}
